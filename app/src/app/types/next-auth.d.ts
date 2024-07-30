ef845441a8dcf2aa42296495ad60cb44f6b620df
dev-shreyash <bhosaleshreyash2@gmail.com>
2024-07-29 21:35:59 -0700

    update: previous setup

---
+++ app/src/app/types/next-auth.d.ts
@@ -0,0 +1,24 @@
+
+import NextAuth, { DefaultSession, DefaultUser, JWT } from 'next-auth';
+import { JWT as NextAuthJWT } from 'next-auth/jwt';
+
+declare module 'next-auth' {
+  interface Session extends DefaultSession {
+    user: {
+      id: string;
+      username: string;
+    } & DefaultSession['user'];
+  }
+
+  interface User extends DefaultUser {
+    id: string;
+    username: string;
+  }
+}
+
+declare module 'next-auth/jwt' {
+  interface JWT extends NextAuthJWT {
+    id: string;
+    username: string;
+  }
+}
