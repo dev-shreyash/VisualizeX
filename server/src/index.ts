4219ef5e9c0b8d2f7e453784c563fd600687afb7
dev-shreyash <bhosaleshreyash2@gmail.com>
2025-01-15 18:33:11 +0530

    setting up  admin page

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,10 @@
 import { Elysia } from "elysia";
 import { executeRouteHandler } from "./controllers/codeExecution";
 import cors from "@elysiajs/cors";
+import "dotenv/config";
 
 const app = new Elysia()
-  .use(cors({ origin: "http://localhost:3000" })) // Allow requests from your frontend URL
+  .use(cors({ origin: process.env.CLIENT_URL })) // Allow requests from your frontend URL
   .get("/", () => "Hello, Elysia!")
   .post("/execute", executeRouteHandler) // Use the handler from executeCode.ts
   .listen(5000); // Set the port to 5000
